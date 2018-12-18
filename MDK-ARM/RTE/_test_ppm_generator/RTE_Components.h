b5f9d5d3d5544db4f0aa296e4f84544f8a253dfe
longhuarst <841105197@qq.com>
2018-12-18 23:37:18 +0800

    longhuarst
    PPM解码器

---
+++ MDK-ARM/RTE/_test_ppm_generator/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'test_ppm_generator' 
+ * Target:  'test_ppm_generator' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/test_ppm_generator/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+test_ppm_generator\startup_stm32f103xe.o: startup_stm32f103xe.s
